44829befe072b7420d28427331abcb1655addbf1
Isaac Mac <isaacmac@iMac.local>
2016-09-28 22:21:53 +0200

    Initial Commit

---
+++ Edad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Edad
+//
+//  Created by Isaac Mac on 28/9/16.
+//  Copyright (c) 2016 Isaac Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
