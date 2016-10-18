b70ec5697760acbba784158ca30dc43d69070cb0
isaaclpz94 <isaaclopez2894@hotmail.com>
2016-10-18 19:15:03 +0200

    Edad
    Calcular la edad introduciendo la fecha. Swift

--- Edad/ViewController.swift
+++ Edad/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var TFFecha: UITextField!
+    @IBOutlet var LBEdad: UILabel!
+    @IBAction func BTNCalcular(sender: UIButton) {
+        var fecha: String = TFFecha.text
+        
+        if !fecha.isEmpty{
+            if isValidEmail(fecha){
+                //Declaramos el formato de la fecha
+                let dateStringFormatter = NSDateFormatter()
+                dateStringFormatter.dateFormat = "dd-MM-yyyy"
+                
+                //La pasamos a NSDate
+                let d1 = dateStringFormatter.dateFromString(fecha)!
+                //Fecha actual
+                let d2 = NSDate()
+                
+                var calendar: NSCalendar = NSCalendar.currentCalendar()
+                
+                // Replace the hour (time) of both dates with 00:00
+                let date1 = calendar.startOfDayForDate(d1)
+                let date2 = calendar.startOfDayForDate(d2)
+                
+                let flags = NSCalendarUnit.CalendarUnitYear
+                let components = calendar.components(flags, fromDate: d1, toDate: d2, options: NSCalendarOptions())
+                
+                LBEdad.text = String(components.year)
+            }else{
+                LBEdad.text = "La fecha no es válida"
+            }
+        }else{
+            LBEdad.text = "Inserte una fecha"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func isValidEmail(testStr:String) -> Bool {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let emailRegEx = "\\d{2}\\/\\d{2}\\/\\d{4}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(testStr)
     }
-
-
 }
 
